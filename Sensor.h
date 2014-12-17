7fd5d7960292b4e5b71768222a88996675b284b6
Alecs Geuder <al.geuder@gmail.com>
2014-12-17 18:29:42 +0000

    removed unused parameter from collectData

--- Sensor.h
+++ Sensor.h
@@ -28,7 +28,7 @@ class Sensor {
 
     // Creates a dataPoint of format "(<sensorName> <timestamp> <value>)" and
     // asks the given Board instance to write it.
-    void collectData(char* filePath, byte filePathLength, char* timestamp, B b) {
+    void collectData(char* filePath, char* timestamp, B b) {
       Serial.println("filepath in collectData: " + String(filePath));
       char dataPoint[MAX_DATAPOINT_SIZE] = { OPEN_DATAPOINT, name };
       strcat(dataPoint, SEPARATOR);
